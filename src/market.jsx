fa979ad8d9f0477204a70d91f1d7bb871fa175a2
최재우 <gggghhjjp@gmail.com>
2026-07-13 16:34:54 +0900

    eni

--- src/market.jsx
+++ src/market.jsx
@@ -1,5 +1,77 @@
+import { useState } from "react";
+
 function Market() {
-  return <></>;
+  const [count, setCount] = useState(0);
+  const [speedcount, setSpeedCount] = useState(0);
+  const [speed, setSpeed] = useState(50);
+  const [goldcount, setGoldCount] = useState(1);
+  const [gold, setGold] = useState(100);
+  const [level, setLevel] = useState(1000);
+  const [leverCount, setLeverCount] = useState(0);
+  const [clickCount, setClickCount] = useState(0);
+  const [needClickCount, setNeedClickCount] = useState(10);
+  return (
+    <>
+      <ul>
+        <li>
+          속도 : {speedcount}
+          <button
+            onClick={() => {
+              if (speedcount < 5 && count >= speed) {
+                setSpeed((speed) => speed + 50);
+                setCount((count) => count - speed);
+                setSpeedCount((speedcount) => speedcount + 1);
+                setNeedClickCount((needClickCount) => needClickCount - 1);
+              } else if (speedcount <= 9 && count >= speed) {
+                setSpeed((speed) => speed + 100);
+                setCount((count) => count - speed);
+                setSpeedCount((speedcount) => speedcount + 1);
+                setNeedClickCount((needClickCount) => needClickCount - 1);
+              } else if (speedcount == 10) {
+                alert("최대 속도에 도달했습니다.");
+              }
+            }}
+          >
+            {speed}
+          </button>
+        </li>
+        <li>
+          골드 수 : {goldcount}개
+          <button
+            onClick={() => {
+              if (count >= gold) {
+                setGold((gold) => gold + 100);
+                setGoldCount((goldcount) => goldcount + 1);
+                setCount((count) => count - gold);
+              }
+            }}
+          >
+            {gold}
+          </button>
+        </li>
+        <li>
+          레벨 : {leverCount}
+          <button
+            onClick={() => {
+              if (leverCount < 5 && count >= level) {
+                setLevel((level) => level + 1000);
+                setCount((count) => count - level);
+                setLeverCount((leverCount) => leverCount + 1);
+              } else if (leverCount <= 9 && count >= level) {
+                setLevel((level) => level + 2000);
+                setCount((count) => count - level);
+                setLeverCount((leverCount) => leverCount + 1);
+              } else if (leverCount == 10) {
+                alert("최대 레벨에 도달했습니다.");
+              }
+            }}
+          >
+            {level}
+          </button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default Market;
